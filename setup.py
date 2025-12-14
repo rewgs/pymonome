88784b8294ee6ee46c27d3c488d972d5c8e16c6d
Alex Ruger <alexrugermusic@gmail.com>
2025-12-13 20:11:15 -0800

    Formatted with black.

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 from setuptools import setup
 
-with open('README.rst') as file:
+with open("README.rst") as file:
     long_description = file.read()
 
 setup(
-    name='pymonome',
-    author='Artem Popov',
-    author_email='artfwo@gmail.com',
-    url='https://github.com/artfwo/pymonome',
-    description='a monome serialosc client in python',
+    name="pymonome",
+    author="Artem Popov",
+    author_email="artfwo@gmail.com",
+    url="https://github.com/artfwo/pymonome",
+    description="a monome serialosc client in python",
     long_description=long_description,
-    version='0.11.2',
-    py_modules=['monome'],
+    version="0.11.2",
+    py_modules=["monome"],
     include_package_data=True,
-    install_requires=[
-        'aiosc'
-    ],
+    install_requires=["aiosc"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Topic :: Multimedia :: Sound/Audio',
-        'Topic :: Software Development :: Libraries',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Software Development :: Libraries",
     ],
-    license='MIT'
+    license="MIT",
 )
